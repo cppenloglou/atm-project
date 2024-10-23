2db45f4f1b0954c4c7d2d2fa4c3b4c5c6e670db5
cppenloglou <xrisostomospenloglou13@gmail.com>
2024-10-24 01:02:29 +0300

    FRONTEND implementation with 2 ways for now and we will see which of those i will keep

---
+++ atm-frontend/components/register-form.tsx
@@ -0,0 +1,65 @@
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function RegisterForm() {
+    return (
+        <Card className="mx-auto max-w-sm">
+            <CardHeader>
+                <CardTitle className="text-2xl">Register</CardTitle>
+                <CardDescription>
+                    Enter your information below to Register to your account
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="grid gap-4">
+                    <div className="grid gap-2">
+                        <Label htmlFor="firstname">Firstname</Label>
+                        <Input
+                            id="firstname"
+                            type="firstname"
+                            placeholder="Jonh"
+                            required
+                        />
+                    </div>
+                    <div className="grid gap-2">
+                        <Label htmlFor="lastname">Lastname</Label>
+                        <Input
+                            id="lastname"
+                            type="lastname"
+                            placeholder="Doe"
+                            required
+                        />
+                    </div>
+                    <div className="grid gap-2">
+                        <Label htmlFor="email">Email</Label>
+                        <Input
+                            id="email"
+                            type="email"
+                            placeholder="m@example.com"
+                            required
+                        />
+                    </div>
+                    <div className="grid gap-2">
+                        <div className="flex items-center">
+                            <Label htmlFor="password">Password</Label>
+                        </div>
+                        <Input id="password" type="password" required/>
+                    </div>
+                    <Button type="submit" className="w-full">
+                        Register
+                    </Button>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
